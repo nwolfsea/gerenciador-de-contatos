29d9b3da2796f11fdeb8dbf9b9a95e2bdd4c00d1
natalia.palota <natalia.palota@zup.com.br>
2021-03-20 18:28:10 -0300

    Adicionar if com respostas para o cadastro

--- src/com/br/zup/Main.java
+++ src/com/br/zup/Main.java
@@ -5,17 +5,48 @@ package com.br.zup;
 // cada contato tem nome, telefone e email.
 // Nenhum programa final deve ser aplicado a uma lista de contatos cadastrados
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 //Entrega mínima
 //
 //sistema permite, via terminal, inserir pelo menos 1 contato em uma lista e retorno o mesmo na tela
+
+//SE inserir contato
+//Então retorna o mesmo na tela
 public class Main {
 
     public static void main(String[] args) {
-        IO entradaESaida = new IO();
-        entradaESaida.pedirUmDado("Digite um nome").nextLine();
-        entradaESaida.pedirUmDado("Digite um email").nextLine();
-        entradaESaida.pedirUmDado("Digite um telefone").nextInt();
+        List<String> cadastroControle = new ArrayList<>();
+
+        boolean continuar = true;
+        while (continuar) {
+            System.out.println("Digite \n1 para cadastrar \n2 para pesquisar lista\n3 para sair");
+            int resposta = entradaDeDados().nextInt();
+
+
+            if (resposta == 1) {
+                System.out.println("Digite o nome");
+                String nome = entradaDeDados().nextLine();
+                List<String> cadastro = new ArrayList<>();
+
+                System.out.println("Digite o email");
+                String email = entradaDeDados().nextLine();
+                cadastro = new ArrayList<>();
+
+                System.out.println("Digite o telefone");
+                int telefone = entradaDeDados().nextInt();
+                cadastro = new ArrayList<>();
+
+            }
+        }
+    }
+
+
+    public static Scanner entradaDeDados() {
+        return new Scanner(System.in);
     }
 }
+
+
